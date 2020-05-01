c72b9263e1f7dc50e6980a179ef1477f4b557dd8
NarawishS <narawish.s@ku.th>
2020-05-01 14:01:53 +0700

    main for run stage

---
+++ src/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Mainview mainview = new Mainview();
+        Scene scene = new Scene(mainview);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        mainview.draw();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
