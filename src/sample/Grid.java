7f2718eb4668cfd00090e4227aa0746aefb027bc
NarawishS <narawish.s@ku.th>
2020-05-01 14:41:01 +0700

    some error thrown when set state of cell fixed

--- src/sample/Grid.java
+++ src/sample/Grid.java
@@ -15,7 +15,7 @@ public class Grid {
         StringBuilder line = new StringBuilder();
         System.out.println("---");
         for (int y = 0; y < height; y++) {
-            line.delete(0,line.length());
+            line.delete(0, line.length());
             line.append("|");
             for (int x = 0; x < width; x++) {
                 if (this.grid[x][y] == 0) {
@@ -31,19 +31,24 @@ public class Grid {
     }
 
     public void setAlive(int x, int y) {
-        this.grid[x][y] = 1;
+        setState(x, y, 1);
     }
 
     public void setDead(int x, int y) {
-        this.grid[x][y] = 0;
+        setState(x, y, 0);
+    }
+
+    public void setState(int x, int y, int state) {
+        if (x < this.width && y < this.height && x > 0 && y > 0)
+            this.grid[x][y] = state;
     }
 
     public int countNeighbors(int x, int y) {
         int sum = 0;
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
-                int col = (x + i + width) % width;
-                int row = (y + j + height) % height;
+                int col = (x + i + this.width) % this.width;
+                int row = (y + j + this.height) % this.height;
                 sum += this.grid[col][row];
             }
         }
@@ -52,10 +57,10 @@ public class Grid {
     }
 
     public void next() {
-        int[][] next = new int[width][height];
+        int[][] next = new int[this.width][this.height];
         // Compute next based on grid
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
                 // Count live neighbors!
                 int neighbors = countNeighbors(i, j);
                 int state = this.grid[i][j];
@@ -69,12 +74,5 @@ public class Grid {
             }
         }
         this.grid = next;
-    }
-
-    public static void main(String[] args) {
-        Grid grid = new Grid(10, 5);
-        grid.setAlive(2, 2);
-        grid.setAlive(3, 2);
-        grid.setAlive(4, 2);
     }
 }
