5668ba498fdbc7d0467e4f8176093f0716eff272
NarawishS <narawish.s@ku.th>
2020-05-12 02:09:31 +0700

    slider, startbutton, stopbutton, randombutton add

--- src/sample/Toolbar.java
+++ src/sample/Toolbar.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.animation.Animation;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -9,6 +10,7 @@ import javafx.scene.layout.HBox;
 
 public class Toolbar extends HBox {
     private final Mainview mainview;
+    private final Slider speedSlider;
 
     public Toolbar(Mainview main) {
         this.mainview = main;
@@ -19,13 +21,35 @@ public class Toolbar extends HBox {
         Button eraseButton = new Button("erase");
         eraseButton.setOnAction(this::eraseHandler);
         Label label1 = new Label("speed");
-        Slider speedSlider = new Slider();
         Button startButton = new Button("start");
+        startButton.setOnAction(this::startHandler);
         Button stopButton = new Button("stop");
+        stopButton.setOnAction(this::stopHandler);
+        Button randomButton = new Button("random");
+        randomButton.setOnAction(this::randomHandler);
+        this.speedSlider = new Slider();
+        this.speedSlider.setMax(500);
+        this.speedSlider.setMin(1);
         this.setAlignment(Pos.CENTER);
         this.setSpacing(5);
-        this.getChildren().addAll(drawButton, eraseButton,
+        this.getChildren().addAll(drawButton, eraseButton, randomButton,
                 label1, speedSlider, startButton, stopButton, stepButton);
+    }
+
+    private void randomHandler(ActionEvent event) {
+        this.mainview.getGrid().randomGrid();
+        this.mainview.draw();
+    }
+
+    private void startHandler(ActionEvent event) {
+        if (this.mainview.getSimulator().getTimelineStat() != Animation.Status.RUNNING) {
+            this.mainview.getSimulator().setTimeline(this.speedSlider.getValue());
+            this.mainview.getSimulator().start();
+        }
+    }
+
+    private void stopHandler(ActionEvent event) {
+        this.mainview.getSimulator().stop();
     }
 
     private void stepHandler(ActionEvent event) {
@@ -34,10 +58,11 @@ public class Toolbar extends HBox {
     }
 
     private void drawHandler(ActionEvent event) {
-        this.mainview.setMode(Grid.ALIVE);
+        this.mainview.setCell(Grid.ALIVE);
     }
 
     private void eraseHandler(ActionEvent event) {
-        this.mainview.setMode(Grid.DEAD);
+        this.mainview.setCell(Grid.DEAD);
     }
+
 }
