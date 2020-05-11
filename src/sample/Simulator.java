0a8f6ec33fc5337cdbfde65ed8bb26f606b5313c
NarawishS <narawish.s@ku.th>
2020-05-12 02:11:01 +0700

    class for Animation

---
+++ src/sample/Simulator.java
@@ -0,0 +1,40 @@
+package sample;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+
+public class Simulator {
+    private Timeline timeline;
+    private final Mainview mainview;
+
+    public Simulator(Mainview mainview) {
+        this.mainview = mainview;
+        setTimeline(500);
+    }
+
+    private void doStep(ActionEvent event) {
+        this.mainview.getGrid().next();
+        this.mainview.draw();
+    }
+
+
+    public void start() {
+        this.timeline.play();
+    }
+
+    public void stop() {
+        this.timeline.stop();
+    }
+
+    public void setTimeline(double num) {
+        this.timeline = new Timeline(new KeyFrame(Duration.millis(num), this::doStep));
+        this.timeline.setCycleCount(Timeline.INDEFINITE);
+    }
+
+    public Animation.Status getTimelineStat() {
+        return this.timeline.getStatus();
+    }
+}
