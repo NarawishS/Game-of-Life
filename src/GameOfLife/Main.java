bde76315c01fb0032f363d29a9ab36bb0edfe3d2
NarawishS <narawish.s@ku.th>
2020-05-12 17:05:43 +0700

    added configuration menu

--- src/GameOfLife/Main.java
+++ src/GameOfLife/Main.java
@@ -7,14 +7,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Mainview mainview = new Mainview();
-        Scene scene = new Scene(mainview);
-        primaryStage.setTitle("Game of life");
-        primaryStage.setResizable(false);
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) {
+        Startview startview = new Startview(primaryStage);
+        Scene startScene = new Scene(startview);
+        primaryStage.setTitle("Game of life: Configuration");
+        primaryStage.setScene(startScene);
         primaryStage.show();
-        mainview.draw();
     }
 
     public static void main(String[] args) {
